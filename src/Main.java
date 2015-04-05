1800c909bbf4a5aeb45536fbacea478d9eca60b0
V Z <vl2z@mail.ru>
2015-04-06 00:45:05 +0300

    Начальная версия программы определения "настроения" текста. Реализовано считывание файла и вывод его содержимого в консоль

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,14 @@ public class Main{
         BufferedReader fin = new BufferedReader(new FileReader(f));
         String name;
         String line;
+        int quantity;
         System.out.println("Print File "+f.getName()+"? y/n");
         name = br.readLine();
         if(name.equals("y"))
             while ((line = fin.readLine()) != null) {
                 System.out.println(line);
-                System.out.println(checkWithRegExp(line));
+
             }
     }
-    public static boolean checkWithRegExp(String userNameString){
-        Pattern p = Pattern.compile("bad\\s|worse[\\t\\n\\x0B\\f\\r]|worst\\s|die\\s", Pattern.CASE_INSENSITIVE) ;
-        Matcher m = p.matcher(userNameString);
-        return m.find() ;
-    }
+
 }
