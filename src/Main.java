6b4fc7849469493f037000671d24634fd71c6478
V Z <vl2z@mail.ru>
2015-04-07 00:08:55 +0300

    Усовершенствован вывод: при попадании отображается паттерн этого попадания, подправлена ф-я checkWithRegExp, убрана лишняя переменная

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,18 @@ import java.io.*;
 
 public class Main{
     static  Pattern[] pAr = new Pattern[] {
-           // Pattern.compile("bad\\s", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("bad\\s", Pattern.CASE_INSENSITIVE),
             Pattern.compile("\\sdie[a-z]*"),
-            Pattern.compile("worse\\s")
+
     };
-    static int full;
+    static int full=0;
     public static void main(String[] args) throws IOException{
-
-        //Pattern p = Pattern.compile("bad\\s|worse\\s|worst\\s|\\sdie[a-z]*|\\ssuffer[a-z]*|mad\\s|crazy\\s|\\snightmare[a-z]*|\\sdeath[a-z]*|\\sfear[a-z]*", Pattern.CASE_INSENSITIVE) ;
-
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         File f = new File("F:/text.txt");
         BufferedReader fin = new BufferedReader(new FileReader(f));
         String name;
         String fl;
         String line;
-        int quantity=0;
         System.out.println("Search in File "+f.getName()+"? y/n");
         name = br.readLine();
         if(name.equals("y")) {
@@ -30,16 +25,11 @@ public class Main{
             if (fl.equals("y"))
                 while ((line = fin.readLine()) != null) {
                     System.out.println(line);
-                    if (checkWithRegExp(line)) {
-                        System.out.println("\t\t TRUE");
-                        quantity++;
-                    }
+                    checkWithRegExp(line);
                 }
             else
                 while ((line = fin.readLine()) != null) {
-                    if (checkWithRegExp(line)) {
-                        quantity++;
-                    }
+                    checkWithRegExp(line);
                 }
                 System.out.println("All negative hits (\"text mood\") = -"+full);
         }
@@ -49,19 +39,19 @@ public class Main{
         Matcher m = p.matcher(userNameString);
         return m.find() ;
     }*/
-    public static boolean checkWithRegExp(String userNameString){
+    public static void checkWithRegExp(String userNameString){
         Matcher m=null;
         boolean b=false;
         for (int j=0; j<pAr.length; j++) {
             m = pAr[j].matcher(userNameString);
             if (m.find()) {
                 full++;
+                System.out.println("\t\t Hit with: " +pAr[j]+" pattern");
                 b=true;
             }
             else
                 b=false;
         }
-        return b;
     }
 
 }
