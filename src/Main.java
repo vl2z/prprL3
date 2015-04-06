bd8cc91adafccfc77cfb1558d32edc395c651b8e
V Z <vl2z@mail.ru>
2015-04-06 23:58:44 +0300

    Исправлена проблема, когда в одной строке находились несколько "нег. слов", а попадание считалось равным 1, а не количеству найденных слов->переделана функция checkWithRegExp

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,17 @@ import java.util.regex.Pattern ;
 import java.io.*;
 
 public class Main{
+    static  Pattern[] pAr = new Pattern[] {
+           // Pattern.compile("bad\\s", Pattern.CASE_INSENSITIVE),
+            Pattern.compile("\\sdie[a-z]*"),
+            Pattern.compile("worse\\s")
+    };
+    static int full;
     public static void main(String[] args) throws IOException{
+
+        //Pattern p = Pattern.compile("bad\\s|worse\\s|worst\\s|\\sdie[a-z]*|\\ssuffer[a-z]*|mad\\s|crazy\\s|\\snightmare[a-z]*|\\sdeath[a-z]*|\\sfear[a-z]*", Pattern.CASE_INSENSITIVE) ;
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         File f = new File("F:/text.txt");
         BufferedReader fin = new BufferedReader(new FileReader(f));
@@ -31,13 +41,27 @@ public class Main{
                         quantity++;
                     }
                 }
-                System.out.println("All negative hits (\"text mood\") = -"+quantity);
+                System.out.println("All negative hits (\"text mood\") = -"+full);
         }
     }
-    public static boolean checkWithRegExp(String userNameString){
+    /*public static boolean checkWithRegExp(String userNameString){
         Pattern p = Pattern.compile("bad\\s|worse\\s|worst\\s|\\sdie[a-z]*|\\ssuffer[a-z]*|mad\\s|crazy\\s|\\snightmare[a-z]*|\\sdeath[a-z]*|\\sfear[a-z]*", Pattern.CASE_INSENSITIVE) ;
         Matcher m = p.matcher(userNameString);
         return m.find() ;
+    }*/
+    public static boolean checkWithRegExp(String userNameString){
+        Matcher m=null;
+        boolean b=false;
+        for (int j=0; j<pAr.length; j++) {
+            m = pAr[j].matcher(userNameString);
+            if (m.find()) {
+                full++;
+                b=true;
+            }
+            else
+                b=false;
+        }
+        return b;
     }
 
 }
